8dd81034900895ada094463414699b45d431db29
Miguel Cueva <miguel.cueva.l@uni.pe>
2024-02-21 12:06:16 -0500

    docs: added dockerfile and kubernetes files

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,9 +1,17 @@
-import { Controller, Get, HttpStatus } from '@nestjs/common';
-import { ApiOkResponse, ApiOperation, ApiTags } from '@nestjs/swagger';
+import { Controller, Get, HttpStatus, UseGuards } from '@nestjs/common';
+import {
+  ApiOkResponse,
+  ApiOperation,
+  ApiSecurity,
+  ApiTags,
+} from '@nestjs/swagger';
 
 import { ApiDocResponse } from './modules/shared/infrastructure/decorators/api-doc-response.decorator';
+import { ApiKeyAuthGuard } from './modules/shared/infrastructure/guards/apikey-auth.guard';
 
 @ApiTags('health')
+@ApiSecurity('Api-Key')
+@UseGuards(ApiKeyAuthGuard)
 @Controller()
 export class AppController {
   @ApiOperation({
